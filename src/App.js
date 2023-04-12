3fa7824324b880f0f6c37fb4114111ace0630ff7
Andreas Lillje <al225wg@student.lnu.se>
2023-04-12 12:00:36 +0200

    Add stats-displayer component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css'
 import Layout from './components/layout/layout.js'
+import StatsDisplayer from './components/stats-displayer/stats-displayer.js'
 
 /**
  *
  */
 function App () {
   return (
-    <Layout />
+    <Layout>
+      <StatsDisplayer />
+      </Layout>
   )
 }
 
---
+++ src/components/stats-displayer/stats-displayer.js
@@ -0,0 +1,17 @@
+import './stats-displayer.css'
+import * as React from 'react'
+
+/**
+ * Stats Displayer Component.
+ * Displays stats.
+ *
+ * @returns {React.ReactElement} - Layout Component.
+ */
+const StastsDisplayer = ({ stat = 'createdat' }) => {
+  return (
+     <div className="stats-displayer-container">
+    </div>
+  )
+}
+
+export default StastsDisplayer
